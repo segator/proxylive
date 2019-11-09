00093f5f1a4030d471b52e75a842efab81ffa806
Segator <isaac.aymerich@gmail.com>
2019-11-09 20:27:48 +0100

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ debug_string=""
 if $DEBUG_MODE ; then
     debug_string="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=6006"
 fi
-java -jar /app/proxyLive.jar $debug_string $@
+java $debug_string -jar /app/proxyLive.jar $@
 
